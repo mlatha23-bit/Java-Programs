c82c0476bf110cc96e47e7bba99d91828058d74a
mlatha23-bit <mlatha2397@gmail.com>
2026-06-29 14:17:16 +0530

    Added Arrays package and ArrayMinMax program

---
+++ src/Arrays/ArrayMinMax.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+class ArrayMinMax 
+{
+	public static void main(String[] args)
+	{
+		int[] num= {12, 45, 7, 89, 23};
+		int Largest_Element=Integer.MIN_VALUE;
+		int Smallest_Element=Integer.MAX_VALUE;
+		for(int i:num)
+		{
+			if(i>Largest_Element)
+			{
+				Largest_Element=i;
+			}
+			
+			if(i<Smallest_Element)
+			{
+				Smallest_Element=i;
+			}
+		}
+		
+		System.out.println("Largest Element= "+Largest_Element);
+		System.out.println("Smallest Element= "+Smallest_Element);
+	
+	}
+}
