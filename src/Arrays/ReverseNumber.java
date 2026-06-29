f76e7cbd9d933ce50a66f30eb294cd8372a065c7
mlatha23-bit <mlatha2397@gmail.com>
2026-06-29 18:00:00 +0530

    Upload - Reverse numbers in an Array - program

---
+++ src/Arrays/ReverseNumber.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+import java.util.Arrays;
+
+class ReverseNumber {
+	public static void main(String[] args)
+	{
+		int[] num= {1, 2, 3, 4, 5};
+		int left=0;
+		int right=num.length-1;
+		while(left<right)
+		{
+			int temp=num[left];
+			num[left]=num[right];
+			num[right]=temp;
+			left++;
+			right--;
+		}
+		
+		System.out.print(Arrays.toString(num));
+	
+			
+	}
+
+}
+
