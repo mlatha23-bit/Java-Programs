2a99dc68cd1f8e3c8b50a84a6a3a520c945553ef
mlatha23-bit <mlatha2397@gmail.com>
2026-06-30 16:47:18 +0530

    Uploaded - Missing Number In An Array - Program

---
+++ src/Arrays/MissingNumber.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+public class MissingNumber {
+	public static void main(String[] args)
+	{
+		int[] num= {1, 2, 3, 8};
+		int n=8;
+		System.out.print("Missing numbers in array:  ");
+		for(int i=1;i<=n;i++)
+		{
+			boolean found=false;
+			for(int j:num)
+			{
+				if(j==i)
+			{
+				found=true;
+				break;
+			}
+		}
+		if(!found)
+		{
+			System.out.print(i+", ");
+			
+		}
+		}
+		
+	}
+
+}
+
