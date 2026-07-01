a651799f155c71ddb4072f83a8e6f7ebe924eee4
mlatha23-bit <mlatha2397@gmail.com>
2026-07-01 14:38:27 +0530

    Created String Package Uploaded - Reverse String - Program

---
+++ src/String/ReverseString.java
@@ -0,0 +1,22 @@
+package String;
+
+public class ReverseString {
+	public static void main(String[] args)
+	{
+		String s="Hello";
+		char[] ch=s.toCharArray();
+		int left=0;
+		int right=ch.length-1;
+		while(left<right)
+		{
+			char temp=ch[left];
+			ch[left]=ch[right];
+			ch[right]=temp;
+			left++;
+			right--;
+		}
+		
+		System.out.println(ch);
+	}
+
+}
