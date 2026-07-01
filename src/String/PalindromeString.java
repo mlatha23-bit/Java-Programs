c969a62c4c50e8a6643096fe1eba55ccee88c862
mlatha23-bit <mlatha2397@gmail.com>
2026-07-01 14:50:38 +0530

    Uploaded - Palindrome String Check - Program

---
+++ src/String/PalindromeString.java
@@ -0,0 +1,32 @@
+package String;
+
+public class PalindromeString {
+	public static void main(String[] args)
+	{
+		String s="Madam";
+		s=s.toLowerCase();
+		char[] ch=s.toCharArray();
+		int i=0;
+		int j=ch.length-1;
+		boolean isPalin=true;
+		while(i<j)
+		{
+			if(ch[i]!=ch[j])
+			{
+				isPalin=false;
+				break;
+			}
+			i++;
+			j--;
+		}
+		if(isPalin)
+		{
+			System.out.println("Palindrome");
+		}
+		else
+		{
+			System.out.println("Not Palindrome");
+		}
+	}
+
+}
