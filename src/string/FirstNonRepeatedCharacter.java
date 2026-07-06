86dbbf29b00d36882b684f72cd88215de3fe558e
mlatha23-bit <mlatha2397@gmail.com>
2026-07-06 15:11:20 +0530

    Uploaded - First Non repeated Character - Program

---
+++ src/string/FirstNonRepeatedCharacter.java
@@ -0,0 +1,28 @@
+package string;
+
+import java.security.KeyStore.Entry;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirstNonRepeatedCharacter {
+public static void main(String[] args) {
+	String s="aabbcdde";
+	
+	Map<Character,Integer> map=new HashMap<>();
+	for(int i=0;i<s.length();i++)
+	{
+		map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
+	}
+	
+	for(int j=0;j<s.length();j++)
+	{
+		if(map.get(s.charAt(j))==1)
+		{
+			System.out.println("First Non repeated Character is: "+s.charAt(j));
+			break;
+		}
+	}
+
+
+}
+}
