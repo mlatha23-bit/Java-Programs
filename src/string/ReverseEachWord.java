6d47747f3522fab0972864f248d3faddc2c39024
mlatha23-bit <mlatha2397@gmail.com>
2026-07-20 11:25:22 +0530

    Uploaded - Reverser Each Word - Without Using reverse() - Program

---
+++ src/string/ReverseEachWord.java
@@ -0,0 +1,20 @@
+package string;
+
+//Without Using reverse()
+public class ReverseEachWord {
+		public static void main(String[] args) {
+		String s="Java Selenium Automation";
+		String[] str=s.split(" ");
+		StringBuilder result=new StringBuilder();
+		for(String i:str)
+		{
+			for(int j=i.length()-1;j>=0;j--)
+			{
+				result.append(i.charAt(j));
+			}
+			result.append(" ");
+		}
+		  System.out.println(result.toString().trim());
+}
+
+}
